8ce589991391828859ee4c873404d80dda056ab7
saif <mohdsaifali708@gmail.com>
2024-03-30 11:43:16 +0530

    final commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import config
-from user_credentials import Credentials
-from login_users import UserLogin
-from product_details import ProductDetails
+import tasks.config as config
+from tasks.user_credentials import Credentials
+from tasks.login_users import UserLogin
+from tasks.product_details import ProductDetails
 
 if __name__ == "__main__":
     credentials_data = Credentials(config.URL, config.FILENAME)
